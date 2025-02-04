9100f50f8ddd929b5c087ef9965ae86034d993bf
wikkiboi <mofouyu@gmail.com>
2025-02-04 03:31:31 -0800

    Add generalErrorHandler & prismaErrorHandler

--- backend/src/middleware/errorHandling/generalError.ts
+++ backend/src/middleware/errorHandling/generalError.ts
@@ -1,1 +1,13 @@
-export default async function generalErrorHandler() {}
+import { NextFunction, Request, Response } from "express";
+import logger from "../../helper/logger";
+
+export default async function generalErrorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  logger.error(`Unhandled error in generalErrorHandler`);
+  logger.error(`${err.message}\n${err.name}\n${err.stack}`);
+  return res.sendStatus(500);
+}
